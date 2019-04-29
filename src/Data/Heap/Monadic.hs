fe40a669ac2f02422a4fba6e3d3e03386981d459
Donnacha Oisín Kidney <mail@doisinkidney.com>
2019-04-29 13:03:36 +0100

    non-internal modules

---
+++ src/Data/Heap/Monadic.hs
@@ -0,0 +1,12 @@
+module Data.Heap.Monadic
+  (Heap
+  ,prio
+  ,Group(..)
+  ,popMin
+  ,prios
+  ,fromList
+  )
+  where
+
+import Data.Group
+import Data.Heap.Monadic.Internal
---
+++ src/Data/Heap/Monadic/Transformer.hs
@@ -0,0 +1,10 @@
+module Data.Heap.Monadic.Transformer
+  (HeapT
+  ,prio
+  ,popMin
+  ,Group(..)
+  )
+  where
+
+import Data.Heap.Monadic.Transformer.Internal
+import Data.Group
